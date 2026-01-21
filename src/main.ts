f0d22f8e3ecde21c2f7b36bd185857ae0757dc75
RzeroDev <hadicodemaster@gmail.com>
2026-01-21 23:40:17 +0100

    Update CORS configuration in main.ts to change the allowed origin from 'https://verrou-dash.vercel.app' to 'https://mini-blog-seven-omega.vercel.app'.

--- src/main.ts
+++ src/main.ts
@@ -16,7 +16,7 @@ async function bootstrap() {
   app.enableCors({
     origin: [
       'http://localhost:5173',
-      'https://verrou-dash.vercel.app'
+      'https://mini-blog-seven-omega.vercel.app'
     ],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     credentials: true,
