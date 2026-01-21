da6b9b4fab174e22deeda1b997b11f95c0ce51ab
RzeroDev <hadicodemaster@gmail.com>
2026-01-22 00:56:06 +0100

    Update admin user's password in seed data for consistency

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -207,7 +207,7 @@ const users = [
     firstName: 'Admin',
     lastName: 'User',
     email: 'admin@mini-blog.com',
-    password: 'admin@mini-blog@1234', // sera hashé
+    password: 'Admin@mini-blog@1234', // sera hashé
     isActive: true,
     isVerified: true,
     roleName: 'admin',
